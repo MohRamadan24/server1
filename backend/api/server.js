43accaa76fe65d27dd805809fa1b8049a306f1d9
MOH RAMADAN <69750539+MohRamadan24@users.noreply.github.com>
2024-07-03 18:50:26 +0700

    run fine in vercel dev

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,44 +1,48 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts') // Adjust path as necessary
-const userRoutes = require('./routes/user') // Adjust path as necessary
-const cors = require('cors')
-const serverless = require('serverless-http')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('../routes/workouts'); // Adjust path as necessary
+const userRoutes = require('../routes/user'); // Adjust path as necessary
+const cors = require('cors');
+const serverless = require('serverless-http');
 
-const app = express()
+const app = express();
 
-const corsOption = {
-  origin: 'https://trinitydev-mernworkout.vercel.app',
-  optionsSuccessStatus: 200
-}
+// CORS configuration to allow all origins
 
-app.use(express.json())
+app.use(cors());
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Request:', req.path, req.method)
-  next()
+  console.log('Request:', req.path, req.method);
+  next();
+});
+
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to database');
+  // listen to port
+  app.listen(process.env.PORT, () => {
+    console.log('Listening for requests on port', process.env.PORT);
+  });
+})
+.catch((err) => {
+  console.error('Error connecting to database:', err.message);
+  process.exit(1); // Exit process on database connection error
+});
+
+app.get("/api/rama/bruh", (req, res) => {
+  res.json({ message: "Hello World!"});
 })
 
-app.use('/api/workouts', cors(corsOption), workoutRoutes)
-app.use('/api/user', cors(corsOption), userRoutes)
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Connected to database')
-  })
-  .catch((err) => {
-    console.error('Database connection error:', err)
-  })
-
-app.get('/', (req, res) => {
-  res.send('Server is running')
-})
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
-
-module.exports.handler = serverless(app)
+// Export app for serverless function
+// module.exports.handler = serverless(app);
+module.exports = app;
