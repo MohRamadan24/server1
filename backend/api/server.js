67579a5339bd69a9f58f7b521e19ec5b8eca9ed2
MOH RAMADAN <69750539+MohRamadan24@users.noreply.github.com>
2024-07-03 16:26:48 +0700

    final, maybe

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,40 +1,41 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const workoutRoutes = require('../routes/workouts')
-const userRoutes = require('../routes/user')
-const cors = require('cors')
-const { default: serverless } = require('serverless-http')
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
+const cors = require('cors');
+const serverless = require('serverless-http');
 
 // express app
-const app = express()
+const app = express();
 
 // cors
 const corsOption = {
   origin: 'https://trinitydev-mernworkout.vercel.app',
   optionsSuccessStatus: 200
-}
+};
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/workouts', cors(corsOption), workoutRoutes)
-app.use('/api/user', cors(corsOption), userRoutes)
+app.use('/api/workouts', cors(corsOption), workoutRoutes);
+app.use('/api/user', cors(corsOption), userRoutes);
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('connected to database')
+    console.log('connected to database');
   })
   .catch((err) => {
-    console.log(err)
-  })
+    console.log(err);
+  });
 
-module.exports.handler = serverless(app)
+// Export the serverless handler
+module.exports.handler = serverless(app);
